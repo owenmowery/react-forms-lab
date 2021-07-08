1f32ff47b56c9be21fa26e1c8c20eef6aa50ef3b
Owen Mowery <owenmowery99@gmail.com>
2021-07-08 14:36:56 -0400

    commit tree fe326bf96ec96d5b2978bce7a1210653b5ae5144

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+        type="text" 
+        name="message" 
+        id="message" 
+        onChange={event => this.handleChange(event)} 
+        value={this.state.message} />
+        {console.log(this.props.maxChar)}
+        <p>{this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
